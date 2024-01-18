0fd878635781c13b83e8eb29e5b3cd12224e4684
sadneo <haywardeel@gmail.com>
2024-01-18 13:53:39 -0500

    Simplified project for now

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,15 +1,9 @@
-use std::iter::Peekable;
-
 #[derive(Debug, PartialEq)]
 pub enum Operator {
     Plus,
     Minus,
     Multiply,
     Divide,
-    Remainder,
-    Power,
-    Factorial,
-    Negative,
 }
 
 #[derive(Debug, PartialEq)]
@@ -22,86 +16,56 @@ pub enum Group {
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Number(f64),
-    Unary(Operator),
     Binary(Operator),
-    Left(Group),
-    Right(Group),
 }
 
-fn lex_number<Iter: Iterator<Item = char>>(char: char, iterator: &mut Peekable<Iter>) -> f64 {
-    let mut buffer = char.to_string();
-    iterator.next();
-
-    while let Some(char) = iterator.peek() {
-        let new = char.to_string();
-        match char {
-            '0'..='9' | '.' => buffer += &new,
-            _ => break,
-        }
-        iterator.next();
-    }
-
-    buffer.parse::<f64>().unwrap_or_default()
-}
-
-pub fn lex(expression: &String) -> anyhow::Result<Vec<Token>> {
-    let mut result: Vec<Token> = vec![];
+pub fn lex(expression: &str) -> anyhow::Result<Vec<Token>> {
+    let mut tokens: Vec<Token> = vec![];
 
     let mut iterator = expression.chars().peekable();
-    while let Some(&char) = iterator.peek() {
+
+    while let Some(char) = iterator.next() {
         match char {
+            '0'..='9' | '.' => {
+                let mut buffer = char.to_string();
+                iterator.next();
+
+                while let Some(new) = iterator.peek() {
+                    match char {
+                        '0'..='9' | '.' => buffer.push(*new),
+                        _ => break,
+                    }
+                    iterator.next();
+                }
+
+                println!("{}", buffer);
+                let number = buffer.parse::<f64>().unwrap();
+                tokens.push(Token::Number(number));
+            },
+            '+' => {
+                tokens.push(Token::Binary(Operator::Plus));
+                iterator.next();
+            },
+            '-' => {
+                tokens.push(Token::Binary(Operator::Minus));
+                iterator.next();
+            },
+            '*' => {
+                tokens.push(Token::Binary(Operator::Multiply));
+                iterator.next();
+            },
+            '/' => {
+                tokens.push(Token::Binary(Operator::Divide));
+                iterator.next();
+            },
             ' ' => {
                 iterator.next();
-            }
-            '0'..='9' | '.' => {
-                let number = lex_number(char, &mut iterator);
-                result.push(Token::Number(number));
-            }
-
-            '+' => {
-                result.push(Token::Binary(Operator::Plus));
-                iterator.next();
-            }
-            '-' => {
-                result.push(Token::Binary(Operator::Minus));
-                iterator.next();
-            }
-            '*' => {
-                result.push(Token::Binary(Operator::Multiply));
-                iterator.next();
-            }
-            '/' => {
-                result.push(Token::Binary(Operator::Divide));
-                iterator.next();
-            }
-            '%' => {
-                result.push(Token::Binary(Operator::Remainder));
-                iterator.next();
-            }
-            '^' => {
-                result.push(Token::Binary(Operator::Power));
-                iterator.next();
-            }
-            '!' => {
-                result.push(Token::Unary(Operator::Factorial));
-                iterator.next();
-            }
-
-            '{' | '[' | '(' => {
-                // i'm just too lazy to make all the match arms rn
-                result.push(Token::Left(Group::Parenthesis));
-                iterator.next();
-            }
-            '}' | ']' | ')' => {
-                result.push(Token::Right(Group::Parenthesis));
-                iterator.next();
-            }
-
+            },
             _ => return Err(anyhow::Error::msg("unknown character in expression")),
         }
     }
 
-    Ok(result)
+    Ok(tokens)
 }
 
 #[cfg(test)]
@@ -113,36 +77,17 @@ mod tests {
     }
 
     #[test]
-    fn partialeq_works() {
-        assert_eq!(Token::Number(5.3), Token::Number(5.3),);
-    }
-    #[test]
-    fn compare_vec_works() {
-        let a = vec![
-            Token::Number(5.3),
-            Token::Number(0.11),
-            Token::Number(330.0),
-        ];
-        let b = vec![
-            Token::Number(5.3),
-            Token::Number(0.11),
-            Token::Number(330.0),
-        ];
-        assert!(compare_vec(&a, &b),);
-    }
-    #[test]
     fn lex_works() {
-        let expression = String::from("(53+110)");
+        let expression = String::from("53+110");
         let equal_to = vec![
-            Token::Left(Group::Parenthesis),
             Token::Number(53.0),
             Token::Binary(Operator::Plus),
             Token::Number(110.0),
-            Token::Right(Group::Parenthesis),
         ];
         let result = lex(&expression).unwrap();
         assert!(compare_vec(&result, &equal_to));
     }
+
     #[test]
     fn lex_decimal() {
         let expression = String::from("5.3 .110 333.");
