d91a8377f363e43dc8387b5802a2d9fa0591fe12
sadneo <haywardeel@gmail.com>
2024-01-21 00:25:05 -0500

    Added simple binary runner for evaluate

---
+++ src/bin.rs
@@ -0,0 +1,11 @@
+use std::io;
+use evaluator::evaluate;
+
+pub fn main() -> anyhow::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    let evaluation = evaluate(buffer.trim_end())?;
+    println!("Evaluation: {}", evaluation);
+    Ok(())
+}
